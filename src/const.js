061952e2664f58c1e2b69ae3e01bc63763bc8f85
Maria-Bogatyreva <63573326+Maria-Bogatyreva@users.noreply.github.com>
2023-05-14 19:44:14 +0700

    4.17. Шаблонизируй то

--- src/const.js
+++ src/const.js
@@ -1,6 +1,14 @@
 const DATE_FORMAT = 'MMM DD';
 const DATE_TIME_FORMAT = 'YY/MM/DD';
+const FILTER_DAY_FORMAT = 'YYYY-MM-DD'
 const DATE_TIME_EDIT_EVENT = 'DD/MM/YY';
 
-export { DATE_FORMAT, DATE_TIME_FORMAT, DATE_TIME_EDIT_EVENT };
+const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past'
+}
+
+export { DATE_FORMAT, DATE_TIME_FORMAT, DATE_TIME_EDIT_EVENT, FilterType, FILTER_DAY_FORMAT };
 
--- src/utils/point.js
+++ src/utils/point.js
@@ -1,10 +1,10 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import {FILTER_DAY_FORMAT} from '../const.js'
 
 import isBetween from 'dayjs/plugin/isBetween';
 dayjs.extend(duration);
 dayjs.extend(isBetween);
-
 
 /* Функция для форматирования даты */
 function humanizeDate(date,dateFormat) {
@@ -18,7 +18,6 @@ const calculateDiffTime = (dateStart, dateEnd) => {
 
   const differentTime = dayjs.duration(end.diff(start));
   const {days, hours, minutes} = differentTime.$d;
-  console.log(differentTime.$d)
   switch (true) {
     case (days > 0):
       return differentTime.format('DD[D] HH[H] mm[M]');
@@ -29,5 +28,31 @@ const calculateDiffTime = (dateStart, dateEnd) => {
   }
 };
 
+const isFuturePoint = (dateStart) => {
+  const formatDateStart = humanizeDate(dateStart, FILTER_DAY_FORMAT);
+  //console.log('дата Начала Больше?', formatDateStart)
+  //console.log('текущей даты', '2023-05-14')
+  return dayjs().isBefore(dayjs(formatDateStart));
+}
 
-export {humanizeDate, calculateDiffTime};
+const isPastPoint = (dateEnd) => {
+  const formatDateEnd = humanizeDate(dateEnd, FILTER_DAY_FORMAT);
+  //console.log('дата Окончания меньше?', formatDateEnd)
+  //console.log('текущей даты', '2023-05-14')
+  return dayjs().isAfter(dayjs(formatDateEnd));
+}
+
+const isPresentPoint = (dateStart, dateEnd) => {
+  const formatDateStart = humanizeDate(dateStart, FILTER_DAY_FORMAT);
+  const formatDateEnd = humanizeDate(dateEnd, FILTER_DAY_FORMAT);
+
+  //console.log('текущая дата', '2023-05-14')
+  //console.log(formatDateStart, 'дата больше текущей?', dayjs().isBefore(dayjs(formatDateStart)))
+  //console.log(formatDateEnd, 'дата меньше текущей?', dayjs().isAfter(dayjs(formatDateEnd)))
+  //console.log('В ПРЕДЕЛАХ?', formatDateStart, '-', formatDateEnd)
+
+  return  dayjs().isAfter(dayjs(formatDateStart)) && dayjs().isBefore(dayjs(formatDateEnd));
+}
+
+
+export {humanizeDate, calculateDiffTime, isFuturePoint, isPastPoint, isPresentPoint};
