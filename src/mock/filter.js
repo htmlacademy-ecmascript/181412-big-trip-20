84c66204b62293ed0718abff7680f03def7ae27a
Maria-Bogatyreva <63573326+Maria-Bogatyreva@users.noreply.github.com>
2023-05-14 19:57:10 +0700

    Доработки

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -4,7 +4,7 @@ function generateFilter(points) {
   return Object.entries(filter).map(
     ([filterType, filterPoints]) => ({
       type: filterType,
-      hasPoints: filterPoints(points).length > 0,
+      hasPoints: filterPoints(points),
     }),
   );
 }
